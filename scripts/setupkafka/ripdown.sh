6817fe03df1dde8bae5aae80d553c50a385e18ed
Phil Prosser <pprosser@redhat.com>
2023-06-01 13:17:52 +0100

    namespace change

--- scripts/setupkafka/ripdown.sh
+++ scripts/setupkafka/ripdown.sh
@@ -6,4 +6,4 @@ oc delete kafkatopic -l strimzi.io/cluster=my-cluster
 # reset heroes DB
 
 export POD=$(oc get pods -l name=heroes-db -n superheroes -o jsonpath="{.items[0].metadata.name}")
-oc exec -it $POD -n superheroes -- bash -c 'export PGUSER=superman ; export PGPASSWORD=superman ; export PGDATABASE=heroes_database ; export PGHOST=heroes-db.default.svc.cluster.local ; psql -c "delete from hero;"'
+oc exec -it $POD -n superheroes -- bash -c 'export PGUSER=superman ; export PGPASSWORD=superman ; export PGDATABASE=heroes_database ; export PGHOST=heroes-db.superheroes.svc.cluster.local ; psql -c "delete from hero;"'
